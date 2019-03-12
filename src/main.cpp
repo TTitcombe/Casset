1f88d8305f0069c3a0dd88a84411729dd6fd95b3
Tom Titcombe <t.j.titcombe@gmail.com>
2019-03-12 10:35:11 +0000

    Add cpr call. Has build errors

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
+#include <map>
+#include <string>
 #include <iostream>
 #include <cpr/cpr.h>
 
 int main(int argc, char **argv) {
-  std::cout << "Hello World" << std::endl;
+  std::cout << "Starting GET req" << std::endl;
+
+  const std::string url = "https://cloud.iexapis.com/beta/tops";
+
+  auto parameters = cpr::Parameters{{"token",""},
+				    {"symbols","aapl"}};
+  auto r = cpr::Get(cpr::Url{url}, parameters);
+
+  std::cout << "Status code: " << r.status_code;
+  std::cout << "Done.";
 }
