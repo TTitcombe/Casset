d37eb0209cedf2939b4187c10499a32a6a7bc561
Tom <32938439+TTitcombe@users.noreply.github.com>
2019-04-02 13:25:03 +0100

    Add Json parser to Casset (#29)
    * Add jsonformoderncpp to conanfile
    
    include json in main.cpp
    
    * parse date from response object using json for modern cpp
    
    * Update README
    
    Use fmt prints instead of std::cout
    
    * Add new conan remote to travis script

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <map>
 #include <string>
+
 #include <cpr/cpr.h>
+#include <nlohmann/json.hpp>
+#include <fmt/format.h>
+
+using json = nlohmann::json;
 
 cpr::Response getChart(const std::string &symbol) {
   std::string url = "https://api.iextrading.com/1.0";
   url += "/stock/"+symbol+"/chart";
 
-  std::cout << "Starting GET req at url " << url << std::endl;
+  fmt::print("Starting GET req at url {}. \n", url);
   return cpr::Get(cpr::Url{url},
                   cpr::VerifySsl{false});  // CPR issue #194
 }
@@ -15,10 +20,14 @@ cpr::Response getChart(const std::string &symbol) {
 int main(int argc, char **argv) {
   auto r = getChart("aapl");
   if (r.status_code == 200) {
-    std::cout << "GET successful." << std::endl;
-    std::cout << "Text " << r.text << std::endl;
+    fmt::print("GET successful.\n");
+
+    auto j3 = json::parse(r.text);
+    const auto date = j3[0]["date"].get<std::string>();
+    fmt::print("Date: {}\n",  date);
+
   } else {
-    std::cout << "GET not successful. Status code is " << r.status_code << std::endl;
+    fmt::print("GET not successful. Status code is {}. \n", r.status_code);
   }
-  std::cout << "Done." << std::endl;
+  fmt::print("Done. \n");
 }
