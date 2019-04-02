196adb7719c886c2131ca4f0e064b6456483812a
Tom Titcombe <t.j.titcombe@gmail.com>
2019-04-02 11:10:21 +0100

    parse date from response object using json for modern cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 
 #include <cpr/cpr.h>
 #include <nlohmann/json.hpp>
+#include <fmt/format.h>
+
+using json = nlohmann::json;
 
 cpr::Response getChart(const std::string &symbol) {
   std::string url = "https://api.iextrading.com/1.0";
@@ -17,10 +20,14 @@ cpr::Response getChart(const std::string &symbol) {
 int main(int argc, char **argv) {
   auto r = getChart("aapl");
   if (r.status_code == 200) {
-    std::cout << "GET successful." << std::endl;
-    std::cout << "Text " << r.text << std::endl;
+    std::cout << "GET successful." << "\n";
+
+    auto j3 = json::parse(r.text);
+    const auto date = j3[0]["date"].get<std::string>();
+    fmt::print("Date: {}",  date);
+
   } else {
     std::cout << "GET not successful. Status code is " << r.status_code << std::endl;
   }
-  std::cout << "Done." << std::endl;
+  std::cout << "Done." << "\n";
 }
