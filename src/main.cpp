f59bc7e3bae6354dabf25fe1ca2d7542b93bf2c1
Tom Titcombe <t.j.titcombe@gmail.com>
2019-04-05 18:59:10 +0100

    Successful compilation with IEX
    Get date from AAPL stock

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,11 @@
-#include "API/IEX.h"
+#include "IEX.h"
 
 #include <iostream>
 
 #include <fmt/format.h>
 
 int main(int argc, char **argv) {
-  const auto iex = API::IEX();
-
-  fmt::print("Enter a stock ticker in uppercase...\n");
-  std::string symbol;
-  std::cin >> symbol;
-
-  const std::string data_date = iex.getChart(symbol);
-  if (data_date != "") {
-    fmt::print("Date for ticker {} is {}.\n", symbol, data_date);
-  } else {
-    fmt::print("Could not retrieve data date.\n");
-  }
+  IEX iex;
+  const std::string date = iex.getChart("AAPL");
+  fmt::print("Date is {}", date);
 }
