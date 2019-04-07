f0d50f358332f2ea4791bfdfddf23e43eba7c0b1
Tom Titcombe <t.j.titcombe@gmail.com>
2019-04-07 15:01:57 +0100

    Add trompeloeil

--- tests/API/IEXTest.cpp
+++ tests/API/IEXTest.cpp
@@ -1,27 +1,18 @@
 #include <catch2/catch.hpp>
+#include <trompeloeil.hpp>
 
 #include "../../src/API/IEX.h"
 
 using json = nlohmann::json;
 
-class IEX_MOCK : API::IEX {
+class IEX_Mock : public API::IEX
+{
 public:
-cpr::Response makeGetRequest(const std::string endpoint, const bool verifySSL) {
-  (void)endpoint;
-  (void)verifySSL;
-  cpr::Response return_object;
-  return return_object;
-}
-
-json parseGetRequest(const cpr::Response &response) {
-  (void)response;
-  json mock_symbols = json::array({ {"AAPL", "APPL_value"}, {"GOOG", "GOOG_VALUE"},
-                                    {"TTT", "TTT_value"}, {"ZZZ", "ZZZ_value"}});
-  return mock_symbols;
-}
+    MAKE_MOCK2(makeGetRequest, cpr::Response(const std::string, const bool));
+    MAKE_MOCK1(parseGetRequest, json(const cpr::Response&));
 };
 
 TEST_CASE( "Test IEX parsing functionality", "[API]") {
-  IEX_MOCK iex_mock;
-  REQUIRE( iex_mock.isValidSymbol("AAPL") == true );
+  IEX_Mock iex_mock;
+  REQUIRE( iex_mock.isValidSymbol("AAPL") == true);
 }
