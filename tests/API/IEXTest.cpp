720da1eeed94457d60d10a22167f2f78ea755b4d
Tom Titcombe <t.j.titcombe@gmail.com>
2019-04-09 17:23:08 +0100

    Mock out parsing in API::IEX
    Remove basic catch test #34

--- tests/API/IEXTest.cpp
+++ tests/API/IEXTest.cpp
@@ -1,18 +1,36 @@
+#define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
-#include <trompeloeil.hpp>
+#include <catch2/trompeloeil.hpp>
 
 #include "../../src/API/IEX.h"
 
-using json = nlohmann::json;
+using trompeloeil::_;
 
 class IEX_Mock : public API::IEX
 {
 public:
-    MAKE_MOCK2(makeGetRequest, cpr::Response(const std::string, const bool));
-    MAKE_MOCK1(parseGetRequest, json(const cpr::Response&));
+    MAKE_MOCK2(getParsedJson, json(const std::string, const bool));
 };
 
-TEST_CASE( "Test IEX parsing functionality", "[API]") {
+TEST_CASE( "Test IEX isValidSymbol", "[API]") {
   IEX_Mock iex_mock;
-  REQUIRE( iex_mock.isValidSymbol("AAPL") == true);
+
+  json json_mock1 = json::array({ {"symbol", "CORRECT_TOKEN"} });
+  json json_mock2 = json::array({ {"symbol", "A_TOKEN"}, {"symbol", "CORRECT_TOKEN"}, {"symbol", "A_THIRD_TOKEN"} });
+  json json_mock3 = json::array({ {"symbol", "A_TOKEN"}, {"symbol", "ANOTHER_TOKEN"}, {"symbol", "A_THIRD_TOKEN"} });
+
+  // Test can find a symbol
+  ALLOW_CALL(iex_mock, getParsedJson(_ , _))
+    .RETURN(json_mock1);
+  REQUIRE(iex_mock.isValidSymbol("CORRECT_TOKEN") == true);
+
+  // test that it loops through the json object until it finds the correct symbol
+  /*ALLOW_CALL(iex_mock, parseGetRequest(_))
+    .RETURN(json_mock2);
+  REQUIRE( iex_mock.isValidSymbol("CORRECT_TOKEN") == true);
+
+  // test that it return false if can't find anywhere
+  ALLOW_CALL(iex_mock, parseGetRequest(_))
+    .RETURN(json_mock1);
+  REQUIRE( iex_mock.isValidSymbol("CORRECT_TOKEN") == false);*/
 }
