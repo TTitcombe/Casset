d8549a0f0b8a1f9d3be8850a8148e9a53caf74ae
Tom Titcombe <t.j.titcombe@gmail.com>
2019-04-10 08:19:50 +0100

    Add mocks which make tests compile

--- tests/API/IEXTest.cpp
+++ tests/API/IEXTest.cpp
@@ -10,27 +10,25 @@ class IEX_Mock : public API::IEX
 {
 public:
     MAKE_MOCK2(getParsedJson, json(const std::string, const bool));
+    // uncomment the following line and the tests will work
+    //MAKE_MOCK1(isValidSymbol, bool(const std::string));
 };
 
 TEST_CASE( "Test IEX isValidSymbol", "[API]") {
   IEX_Mock iex_mock;
 
   json json_mock1 = json::array({ {"symbol", "CORRECT_TOKEN"} });
-  json json_mock2 = json::array({ {"symbol", "A_TOKEN"}, {"symbol", "CORRECT_TOKEN"}, {"symbol", "A_THIRD_TOKEN"} });
-  json json_mock3 = json::array({ {"symbol", "A_TOKEN"}, {"symbol", "ANOTHER_TOKEN"}, {"symbol", "A_THIRD_TOKEN"} });
+  //json json_mock2 = json::array({ {"symbol", "A_TOKEN"}, {"symbol", "CORRECT_TOKEN"}, {"symbol", "A_THIRD_TOKEN"} });
+  //json json_mock3 = json::array({ {"symbol", "A_TOKEN"}, {"symbol", "ANOTHER_TOKEN"}, {"symbol", "A_THIRD_TOKEN"} });
 
   // Test can find a symbol
   ALLOW_CALL(iex_mock, getParsedJson(_ , _))
     .RETURN(json_mock1);
+
+  // uncomment the following ALLOW_CALL and the tests will work
+  /*
+  ALLOW_CALL(iex_mock, isValidSymbol(_))
+    .RETURN(true);
+   */
   REQUIRE(iex_mock.isValidSymbol("CORRECT_TOKEN") == true);
-
-  // test that it loops through the json object until it finds the correct symbol
-  /*ALLOW_CALL(iex_mock, parseGetRequest(_))
-    .RETURN(json_mock2);
-  REQUIRE( iex_mock.isValidSymbol("CORRECT_TOKEN") == true);
-
-  // test that it return false if can't find anywhere
-  ALLOW_CALL(iex_mock, parseGetRequest(_))
-    .RETURN(json_mock1);
-  REQUIRE( iex_mock.isValidSymbol("CORRECT_TOKEN") == false);*/
 }
