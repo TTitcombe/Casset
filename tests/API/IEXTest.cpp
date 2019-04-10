7fa86464d1ce4497cc19f98a7d2eb2505ff58bc8
Tom Titcombe <t.j.titcombe@gmail.com>
2019-04-10 16:04:02 +0100

    Add additional tests
    All still fail

--- tests/API/IEXTest.cpp
+++ tests/API/IEXTest.cpp
@@ -4,20 +4,16 @@
 
 #include "../../src/API/IEX.h"
 
-using trompeloeil::_;
-
 class IEX_Mock : public API::IEX
 {
 public:
-    MAKE_MOCK2(getParsedJson, json(const std::string, const bool));i
+    MAKE_MOCK2(getParsedJson, json(const std::string, const bool));
 };
 
 TEST_CASE( "Test IEX isValidSymbol", "[API]") {
   IEX_Mock iex_mock;
   {
     json json_mock1 = json::array({{"symbol", "CORRECT_TOKEN"}});
-    //json json_mock2 = json::array({ {"symbol", "A_TOKEN"}, {"symbol", "CORRECT_TOKEN"}, {"symbol", "A_THIRD_TOKEN"} });
-    //json json_mock3 = json::array({ {"symbol", "A_TOKEN"}, {"symbol", "ANOTHER_TOKEN"}, {"symbol", "A_THIRD_TOKEN"} });
 
     // Test can find a symbol
     REQUIRE_CALL(iex_mock, getParsedJson("/ref-data/symbols", false))
@@ -25,5 +21,25 @@ TEST_CASE( "Test IEX isValidSymbol", "[API]") {
 
     const bool return_val = iex_mock.isValidSymbol("CORRECT_TOKEN");
     REQUIRE(return_val == true);
+  }
+  {
+    json json_mock2 = json::array({ {"symbol", "A_TOKEN"}, {"symbol", "CORRECT_TOKEN"}, {"symbol", "A_THIRD_TOKEN"} });
+
+   // Test can loops through the json object until the correct symbol is found
+    REQUIRE_CALL(iex_mock, getParsedJson("/ref-data/symbols", false))
+      .RETURN(json_mock2);
+
+    const bool return_val = iex_mock.isValidSymbol("CORRECT_TOKEN");
+    REQUIRE(return_val == true);
+  }
+  {
+   json json_mock3 = json::array({ {"symbol", "A_TOKEN"}, {"symbol", "ANOTHER_TOKEN"}, {"symbol", "A_THIRD_TOKEN"} });
+
+    // Test will return false if symbol is not found in json object
+    REQUIRE_CALL(iex_mock, getParsedJson("/ref-data/symbols", false))
+      .RETURN(json_mock3);
+
+    const bool return_val = iex_mock.isValidSymbol("CORRECT_TOKEN");
+    REQUIRE(return_val == false);
   }
 }
