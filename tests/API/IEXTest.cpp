095717801ef3ece7d0686848517d47e230fdcce2
Tom Titcombe <t.j.titcombe@gmail.com>
2019-04-10 15:59:58 +0100

    Add IEX.cpp to UnitTests target
    Tests run but fail

--- tests/API/IEXTest.cpp
+++ tests/API/IEXTest.cpp
@@ -9,26 +9,21 @@ using trompeloeil::_;
 class IEX_Mock : public API::IEX
 {
 public:
-    MAKE_MOCK2(getParsedJson, json(const std::string, const bool));
-    // uncomment the following line and the tests will work
-    //MAKE_MOCK1(isValidSymbol, bool(const std::string));
+    MAKE_MOCK2(getParsedJson, json(const std::string, const bool));i
 };
 
 TEST_CASE( "Test IEX isValidSymbol", "[API]") {
   IEX_Mock iex_mock;
+  {
+    json json_mock1 = json::array({{"symbol", "CORRECT_TOKEN"}});
+    //json json_mock2 = json::array({ {"symbol", "A_TOKEN"}, {"symbol", "CORRECT_TOKEN"}, {"symbol", "A_THIRD_TOKEN"} });
+    //json json_mock3 = json::array({ {"symbol", "A_TOKEN"}, {"symbol", "ANOTHER_TOKEN"}, {"symbol", "A_THIRD_TOKEN"} });
 
-  json json_mock1 = json::array({ {"symbol", "CORRECT_TOKEN"} });
-  //json json_mock2 = json::array({ {"symbol", "A_TOKEN"}, {"symbol", "CORRECT_TOKEN"}, {"symbol", "A_THIRD_TOKEN"} });
-  //json json_mock3 = json::array({ {"symbol", "A_TOKEN"}, {"symbol", "ANOTHER_TOKEN"}, {"symbol", "A_THIRD_TOKEN"} });
+    // Test can find a symbol
+    REQUIRE_CALL(iex_mock, getParsedJson("/ref-data/symbols", false))
+      .RETURN(json_mock1);
 
-  // Test can find a symbol
-  ALLOW_CALL(iex_mock, getParsedJson(_ , _))
-    .RETURN(json_mock1);
-
-  // uncomment the following ALLOW_CALL and the tests will work
-  /*
-  ALLOW_CALL(iex_mock, isValidSymbol(_))
-    .RETURN(true);
-   */
-  REQUIRE(iex_mock.isValidSymbol("CORRECT_TOKEN") == true);
+    const bool return_val = iex_mock.isValidSymbol("CORRECT_TOKEN");
+    REQUIRE(return_val == true);
+  }
 }
