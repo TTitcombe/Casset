c53e5be81bd34320a08a181f80841a300f497153
Tom Titcombe <t.j.titcombe@gmail.com>
2019-04-16 08:43:14 +0100

    Change to bdd test style
    remove unnecessary find package in cmakelists
    
    re #34

--- tests/API/IEXTest.cpp
+++ tests/API/IEXTest.cpp
@@ -7,39 +7,42 @@
 class IEX_Mock : public API::IEX
 {
 public:
-    MAKE_MOCK2(getParsedJson, json(const std::string, const bool), override);
+    MAKE_MOCK2(getParsedJson, json(const std::string &, const bool), override);
 };
 
-TEST_CASE( "Test IEX isValidSymbol", "[API]") {
-  IEX_Mock iex_mock;
-  {
-    json json_mock1 = json::parse("[{\"symbol\": \"CORRECT_TOKEN\"}]");
+SCENARIO("JSON objects can be searched for a user-provided company ticker", "[API][IEX]") {
+    IEX_Mock iex_mock;
 
-    // Test can find a symbol
-    REQUIRE_CALL(iex_mock, getParsedJson("/ref-data/symbols", false))
-      .RETURN(json_mock1);
+    GIVEN("An array containing a single JSON object") {
+        json json_mock = json::parse("[{\"symbol\": \"CORRECT_TOKEN\"}]");
+        WHEN("We search for the ticker symbol present in the JSON object") {
+            REQUIRE_CALL(iex_mock, getParsedJson("/ref-data/symbols", false))
+              .RETURN(json_mock);
 
-    const bool return_val = iex_mock.isValidSymbol("CORRECT_TOKEN");
-    REQUIRE(return_val == true);
-  }
-  {
-    json json_mock2 = json::parse("[{\"symbol\": \"A_TOKEN\"}, {\"symbol\": \"CORRECT_TOKEN\"}, {\"symbol\": \"A_THIRD_TOKEN\"}]");
+            const bool ticker_was_found = iex_mock.isValidSymbol("CORRECT_TOKEN");
+            THEN("The ticker symbol should have been found") {
+                REQUIRE(ticker_was_found);
+            }
+        }
+    }
+    GIVEN("An array containing multiple JSON objects") {
+        json json_mock = json::parse("[{\"symbol\": \"A_TOKEN\"}, {\"symbol\": \"CORRECT_TOKEN\"}, {\"symbol\": \"A_THIRD_TOKEN\"}]");
+        WHEN("We search for a ticker symbol present in one of the JSON objects") {
+            REQUIRE_CALL(iex_mock, getParsedJson("/ref-data/symbols", false))
+              .RETURN(json_mock);
 
-   // Test can loops through the json object until the correct symbol is found
-    REQUIRE_CALL(iex_mock, getParsedJson("/ref-data/symbols", false))
-      .RETURN(json_mock2);
-
-    const bool return_val = iex_mock.isValidSymbol("CORRECT_TOKEN");
-    REQUIRE(return_val == true);
-  }
-  {
-   json json_mock3 = json::parse("[{\"symbol\": \"A_TOKEN\"}, {\"symbol\": \"ANOTHER_TOKEN\"}, {\"symbol\": \"A_THIRD_TOKEN\"}]");
-
-    // Test will return false if symbol is not found in json object
-    REQUIRE_CALL(iex_mock, getParsedJson("/ref-data/symbols", false))
-      .RETURN(json_mock3);
-
-    const bool return_val = iex_mock.isValidSymbol("CORRECT_TOKEN");
-    REQUIRE(return_val == false);
-  }
+            const bool ticker_was_found = iex_mock.isValidSymbol("CORRECT_TOKEN");
+            THEN("The ticker symbol should have been found") {
+                REQUIRE(ticker_was_found);
+            }
+         }
+        WHEN("We search for a ticker symbol not present in the JSON objects") {
+            REQUIRE_CALL(iex_mock, getParsedJson("/ref-data/symbols", false))
+              .RETURN(json_mock);
+            const bool ticker_was_found = iex_mock.isValidSymbol("AN_INCORRECT_TOKEN");
+            THEN("The ticker symbol should not have been found") {
+                REQUIRE(!ticker_was_found);
+            }
+        }
+    }
 }
