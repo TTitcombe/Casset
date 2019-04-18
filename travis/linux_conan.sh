5273b48b606ab703a40f922b33cfa63651e716fa
Tom Titcombe <t.j.titcombe@gmail.com>
2019-04-18 09:50:18 +0100

    Move conan installation commands to separate bash script

---
+++ travis/linux_conan.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -ex
+
+# A bash script to install conan and conan remotes on a linux Travis build
+
+sudo pip3 install conan
+conan user
+
+# Add the remotes
+conan remote add zimmerk https://api.bintray.com/conan/zimmerk/conan
+conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
+conan remote add vthiery https://api.bintray.com/conan/vthiery/conan-packages
+conan remote add cli11 https://api.bintray.com/conan/cliutils/CLI11
+
+mkdir -p ~/.conan/profiles
+cp travis/default ~/.conan/profiles/default
